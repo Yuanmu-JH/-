cb25fe6bd7dead31db65f910cdd9b1c375282e1e
Yuanmu-JH <hahayuanmu@163.com>
2020-12-02 16:57:20 +0800

    update

--- src/Tree/TreePostOrder.java
+++ src/Tree/TreePostOrder.java
@@ -1,26 +1,23 @@
 package Tree;
 
-import javax.swing.tree.TreeNode;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
-/**
-        * Definition for a binary tree node.
-        * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+public class TreeNode {
+    int val;
+   TreeNode left;
+   TreeNode right;
+   TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class TreePostOrder {
     public List<Integer> postorderTraversal(TreeNode root) {
         TreeNode p = root,r = null;
