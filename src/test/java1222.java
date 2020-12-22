4566cb43e25dbf0ee30a2f1e131a1453868fa4e0
Yuanmu-JH <hahayuanmu@163.com>
2020-12-22 16:27:40 +0800

    数组

---
+++ src/test/java1222.java
@@ -0,0 +1,14 @@
+package test;
+
+public class java1222 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+//        for (int i = 0; i < arr.length;i++){
+//            System.out.print(arr[i]);
+//        }
+        //for-each
+        for (int x : arr) {
+            System.out.print(x);
+        }
+    }
+}
