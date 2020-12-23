a1ae38a95e2f70233b437f5ee12c2758d5bed729
Yuanmu-JH <hahayuanmu@163.com>
2020-12-23 10:40:24 +0800

    数组

--- src/test/java1222.java
+++ src/test/java1222.java
@@ -3,10 +3,10 @@ package test;
 public class java1222 {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
-//        for (int i = 0; i < arr.length;i++){
-//            System.out.print(arr[i]);
-//        }
-        //for-each
+        for (int i = 0; i < arr.length;i++){
+            System.out.print(arr[i]);
+        }
+
         for (int x : arr) {
             System.out.print(x);
         }
