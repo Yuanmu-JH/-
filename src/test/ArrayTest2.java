75918927a569efb46454206428f770a58ee0a902
Yuanmu-JH <hahayuanmu@163.com>
2021-01-17 17:38:12 +0800

    update

--- src/test/ArrayTest2.java
+++ src/test/ArrayTest2.java
@@ -74,7 +74,7 @@ public class ArrayTest2 {
         }
     }
 
-    //判断数组是否有序
+    //判断数组是否有序。
     public static boolean isSorted(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             if(arr[i] >arr[i+1]){
